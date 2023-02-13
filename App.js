fdbfbf094828d9ddf469b2047d11804d2e46a57b
kkuismin <katariina.kuismin@gmail.com>
2023-02-13 17:56:07 +0200

    Exercise done

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, Text, Button, View, FlatList, Image } from 'react-native';
 
 export default function App() {
+
+  const [ingredient, setIngredient] = useState('');
+  const [meals, setMeals] = useState('');
+
+  const getMeals = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+    .then(response => response.json())
+    .then(data => setMeals(data.meals))
+    .catch(error => Alert.alert('Error', error));
+  }
+
+  const ItemSeparator = () => <View
+    style={{
+      height: 1,
+      width: "100%",
+      backgroundColor: "lightgrey",
+      marginBottom: 10,
+    }}
+  />
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <FlatList 
+        style={styles.list}
+        keyExtractor={item => item.idMeal}
+        data={meals}  
+        renderItem={({item}) =>
+          <View>
+            <Text style={styles.text}>{item.strMeal}</Text>
+            <Image style={styles.img} source={{ uri: item.strMealThumb }} />
+          </View>} 
+        ItemSeparatorComponent={ItemSeparator} /> 
+      <TextInput 
+        style={styles.input} 
+        placeholder='ingredient'
+        onChangeText={text => setIngredient(text) } 
+        value={ingredient}/>
+      <Button title="Find" onPress={getMeals} />
     </View>
   );
 }
@@ -16,5 +52,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
+  },
+  list: {
+    marginTop: 20,
+    width: '100%',
+  },
+  img: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+  }, 
+  input: {
+    width: 200,
+    fontSize: 18,
+    padding: 5,
   },
 });
